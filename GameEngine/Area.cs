5a58f03a76b33e5c1355c8d0589401ad5bd7a2f3
Håkon Hjelmeland Dale <haakonhd@hiof.no>
2020-02-18 14:24:28 +0100

    Shake it

---
+++ GameEngine/Area.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameEngine
+{
+    public class Area
+    {
+        public Cell[,] AreaGrid
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ GameEngine/Music.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+
+namespace GameEngine
+{
+    public class Music
+    {
+        private MediaPlayer _mediaPlayer;
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "<Pending>")]
+        public Music()
+        {
+            _mediaPlayer = new MediaPlayer
+            {
+                Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/shake.mp3"))
+            };
+        }
+
+        public void PlayMusic()
+        {
+            _mediaPlayer.Play();
+        }
+
+
+        public void PauseMusic()
+        {
+            _mediaPlayer.Pause();
+        }
+    }
+}

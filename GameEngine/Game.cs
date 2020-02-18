48f9bb3b9b12ffffa3d8e99d3162e8499628ca89
Håkon Hjelmeland Dale <haakonhd@hiof.no>
2020-02-18 12:40:23 +0100

    Initial commit 2

---
+++ GameEngine/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameEngine
+{
+    public class Game
+    {
+
+        public void SetMusic()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetGameName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

be3bb8f616f4c8ea053dfc0c9e85975dacefbe48
TheWoakus <Magnusmn@live.no>
2020-05-11 20:39:30 +0200

    Added summary to atleast one the classes
    A certain someone needs to push their changes so i can see what other classes this is needed on

--- GameEngine/GameBoard/Movement.cs
+++ GameEngine/GameBoard/Movement.cs
@@ -7,8 +7,17 @@ using Windows.System;
 
 namespace GameEngine.GameBoard
 {
+    /// <summary>
+    /// A class used to manage movement of the player
+    /// </summary>
 	public static class Movement
 	{
+        /// <summary>
+        /// A function used to move a character in different directions based on user input
+        /// </summary>
+        /// <param name="cellObject">The object that is supposed to be moved</param>
+        /// <param name="area">What area the object is in</param>
+        /// <param name="direction">Keyboard input that determines the direction to move</param>
         public static void MoveCellObject(ICellObject cellObject, Area area, VirtualKey direction)
 		{
             var originalXCoordinate = cellObject.Position.x;
@@ -66,6 +75,13 @@ namespace GameEngine.GameBoard
             TriggerEventHandlerInNextCellObject(nextXCoordinate, nextYCoordinate, area, GameEvent.EventTypes.Enter);
         }
 
+        /// <summary>
+        /// A function to trigger the events in the next cell
+        /// </summary>
+        /// <param name="xPos">X position of the cell</param>
+        /// <param name="yPos">Y position of the cell</param>
+        /// <param name="area">What area the cell is in</param>
+        /// <param name="eventType">What kind of event type it's going to trigger</param>
         private static void TriggerEventHandlerInNextCellObject(int xPos, int yPos, Area area, GameEvent.EventTypes eventType)
         {
             var cellObjects = area.AreaGrid[xPos - 1][yPos - 1].CellObjects;
@@ -88,6 +104,13 @@ namespace GameEngine.GameBoard
             }
         }
 
+        /// <summary>
+        /// Checks if the next cell the character is attempting to move into is enterable
+        /// </summary>
+        /// <param name="xPos">X position of the tested cell</param>
+        /// <param name="yPos">Y position of the tested cell</param>
+        /// <param name="area">The area the cell is in</param>
+        /// <returns></returns>
         private static bool CheckIfNextCellIsPassable(int xPos, int yPos, Area area)
         {
             if (xPos == 0 || yPos == 0 || xPos == area.AreaGrid.Length+1 || yPos == area.AreaGrid[0].Length+1)
@@ -100,6 +123,11 @@ namespace GameEngine.GameBoard
             return cellObjects.All(cellObject => cellObject.IsPassable);
         }
 
+        /// <summary>
+        /// A function to try to interact with the cells around the player
+        /// </summary>
+        /// <param name="cellObject">The object that you control</param>
+        /// <param name="area">The area the object is in</param>
         public static void InteractWithCellObject(ICellObject cellObject, Area area)
         {
             int xPos = cellObject.Position.x;
@@ -118,6 +146,11 @@ namespace GameEngine.GameBoard
                 TriggerEventHandlerInNextCellObject(xPos,yPos+1,area, GameEvent.EventTypes.Interaction);
         }
 
+        /// <summary>
+        /// A function that will handle the input of the player and use it accordingly
+        /// </summary>
+        /// <param name="eVirtualKey">Keybaord input</param>
+        /// <param name="cellObject">The object that the player controls</param>
         public static void HandleInput(VirtualKey eVirtualKey, ICellObject cellObject)
         {
             switch (Game.Instance.CurrentGameState)
@@ -144,6 +177,11 @@ namespace GameEngine.GameBoard
             }
         }
 
+        /// <summary>
+        /// A function to check if the keyboard input is directional
+        /// </summary>
+        /// <param name="eVirtualKey">Keyboard input</param>
+        /// <returns></returns>
         private static bool IsDirectional(VirtualKey eVirtualKey)
         {
             if (eVirtualKey == VirtualKey.Left
